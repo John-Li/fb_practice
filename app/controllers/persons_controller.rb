308d2bd826ff718aaa8b286dbdcec9983f57b981
John Li <ivan.lishchinovskiy@gmail.com>
2012-06-06 17:22:13 +0300

    added sort for name and id

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,16 +1,21 @@
 class PersonsController < ApplicationController
+  respond_to :json, :html
 
   def index
+    @people = if %w{name id}.include? params[:field] and %w{asc desc}.include? params[:order]
+                Person.order("#{params[:field]} #{params[:order]}") 
+              else
+                Person.all
+              end
+    
+    respond_with @people  
   end
   
   def show
+    #TODO add order id ASC
     @person = Person.where("id > ?", params[:id]).first
-    respond_to do |format|
-      format.html 
-      format.json do 
-        render :json => @person.to_json
-      end
-    end
+    
+    respond_with @person
   end
 end
 
