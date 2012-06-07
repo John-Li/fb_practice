e6efe53c5021ff1a9077206ed1e62a3062737dd1
John Li <ivan.lishchinovskiy@gmail.com>
2012-06-07 18:01:07 +0300

    got the favourites association right

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,9 @@
 class Person < ActiveRecord::Base
   attr_accessible :name
   
-  has_many :person_favourites, foreign_key: "person_id", class_name: "PersonFavourites", dependent: :destroy
-  has_many :favourites, through: :person_favourites, source: "favourites_id"
-  has_many :in_favourites, through: :person_favourites, source: "person_id"
+  has_many :person_favourites, class_name: "PersonFavourites", dependent: :destroy
+  has_many :favourites, through: :person_favourites, foreign_key: :favourites_id
+  has_many :in_favourites, through: :person_favourites # DOTO reflect this in the correct association: SELECT `people`.* FROM `people` INNER JOIN `person_favourites` ON `people`.`id` = `person_favourites`.`favourites_id` WHERE `person_favourites`.`person_id` = 210;
   
   def add_to_favourites!(other_person)
     person_favourites.create!(favourites_id: other_person.id)
