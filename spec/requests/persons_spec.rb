f4c3a3b43e680ed3c92f99dfffe325e66ca4592f
John Li <ivan.lishchinovskiy@gmail.com>
2012-06-20 17:02:59 +0300

    working with Pesons specs

--- spec/requests/persons_spec.rb
+++ spec/requests/persons_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe "Persons" do
-  let(:roy) {create(:person)}
-  let(:rob) {create(:person, name: "Rob")}
-  let(:sam) {create(:person, name: "Sam")}
+  let(:roy) {mock_model(Person, name: 'Roy')}
+  let(:rob) {mock_model(Person, name: "Rob")}
+  let(:sam) {mock_model(Person, name: "Sam")}
+  let(:first_favourite) {roy.stub!(:add_to_favourites!).and_return(mock_model(FavouritesRelation, favourite_id: roy.id, favourites_id: rob.id))}
+  let(:second_favourite) {roy.stub!(:add_to_favourites!).and_return(mock_model(FavouritesRelation, favourite_id: roy.id, favourites_id: sam.id))}
   
-  describe "GET /persons" do
+  
+  describe "Persons index page" do
     it "should GET the page" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get persons_path
@@ -23,10 +26,34 @@ describe "Persons" do
     end
   end
   
-  describe "GET /persons/:id" do
+  describe "Person show page" do
     it "should GET the page" do
       get person_path roy
       response.status.should be(200)
+    end
+  end
+  
+  describe "Person page" do
+    before do
+      first_favourite
+      second_favourite
+      visit person_path roy
+    end
+    
+    it "should have person's name" do
+      page.should have_selector(".name .current", text: roy.name)
+    end
+    
+    it "should have person's favourites" do
+      page.should have_selector(".favourites a.follow", text: rob.name)
+    end
+    
+    it "should have person's in favourites of" do 
+      page.should have_selector(".in_favourites a.follow", text: sam.name)
+    end
+    
+    it "shoud have list of persons for adding to favourites" do
+      page.should have_selector("select#favourite")
     end
   end
   
