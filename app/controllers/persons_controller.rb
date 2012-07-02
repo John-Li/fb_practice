b0c2fa28794cd9e30769f96025fc77ca0c078231
John Li <ivan.lishchinovskiy@gmail.com>
2012-07-02 15:07:33 +0300

    some changes for selction of a person by the first letter in the name

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -4,15 +4,15 @@ class PersonsController < ApplicationController
   respond_to :json, :html
 
   def index
-    # @letters_for_select = ""
-    # ('A'..'Z').each {|letter| @letters_for_select << "<option>#{letter}</option>"}
+    @letters_for_select = ""
+    ('A'..'Z').each {|letter| @letters_for_select << "<option>#{letter}</option>"}
 
-    # @people = if ('A'..'Z').include? params[:first_letter]
-    #             Person.where("name like ?", "params[:first_letter]%").paginate(:page => params[:page], :per_page => 15)
-    #           else
-    #             Person.by_order(params[:field], params[:order]).paginate(:page => params[:page], :per_page => 15)
-    #           end
-    @people = Person.all.paginate(:page => params[:page], :per_page => 15)
+    @people = if ('A'..'Z').include? params[:first_letter]
+                Person.where("name like ?", "#{params[:first_letter]}%").paginate(:page => params[:page], :per_page => 15)
+              else
+                Person.by_order(params[:field], params[:order]).paginate(:page => params[:page], :per_page => 15)
+              end
+    # @people = Person.all.paginate(:page => params[:page], :per_page => 15)
       
     respond_with @people  
   end
